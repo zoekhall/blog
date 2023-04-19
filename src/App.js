477c9bc11dbc3a1967f15f3e3b10375f17c934dc
zoekhall <zoe.k.hall@gmail.com>
2023-04-18 20:15:23 -0500

    added reactRouter paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ArticlesListPage from './components/ArticlesListPage';
+import ArticlePage from './components/ArticlePage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My Awesome Blog</h1>
-      <div id="page-body">
-        Welcome to my blog! 
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />}  />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Article.js
+++
@@ -1,5 +0,0 @@
-const Article = () => {
-	return <h1>This is the article page!</h1>;
-};
-
-export default Article;
