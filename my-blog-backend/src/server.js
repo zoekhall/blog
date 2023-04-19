2565ba2abe1df0b42b7aec7f495fc890acab12e5
zoekhall <zoe.k.hall@gmail.com>
2023-04-18 22:24:05 -0500

    added express

---
+++ my-blog-backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json()); 
+
+app.post('/hello', (req, res) => {
+  res.send(`Hello ${req.body.name}`)
+});
+
+//tell server to listen 
+app.listen(8000, () => { console.log('Server is listening on port 8000') });
