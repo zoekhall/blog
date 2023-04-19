e393286ab323db02b890571e506fc86447ea7afd
zoekhall <zoe.k.hall@gmail.com>
2023-04-18 21:10:10 -0500

    added lorem ipsum to home and about

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,54 @@
 const HomePage = () => {
-	return <h1>This is the home page!</h1>;
+	return (
+		<>
+			<h1>This is the home page!</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut
+				consequat velit. Duis orci nunc, bibendum et accumsan nec, mollis at
+				enim. Aliquam varius, urna sed dignissim placerat, leo diam vestibulum
+				libero, et iaculis massa tortor vitae massa. Ut pellentesque urna
+				aliquam mi aliquam, ut porttitor dolor maximus. Nullam eu commodo
+				lectus. Suspendisse at nulla vel ipsum volutpat pulvinar. Suspendisse
+				non nibh id massa pharetra elementum. Sed orci magna, ornare non lacinia
+				elementum, sodales in nisl. Aliquam pretium elit neque, ac placerat
+				risus malesuada in.
+			</p>
+			<p>
+				Suspendisse non ante molestie, ornare est eu, ullamcorper erat.
+				Suspendisse potenti. Ut eleifend congue mi sed luctus. In tempor sit
+				amet nunc ut tempus. Proin porta, sapien vulputate pharetra maximus,
+				diam orci gravida nisl, eget tincidunt mauris lacus ut tortor. Phasellus
+				dictum eros eu ante dapibus, eu condimentum erat sodales. Phasellus
+				aliquet magna at justo bibendum, id hendrerit leo luctus. Proin in lorem
+				id tellus venenatis blandit a sed nisi. Proin varius nulla ut fringilla
+				tempus. Duis quis sem quis felis semper pharetra. Mauris volutpat neque
+				eget ex aliquam, sit amet euismod justo consequat.
+			</p>
+			<p>
+				Etiam faucibus imperdiet hendrerit. Donec laoreet vitae enim vel ornare.
+				Curabitur viverra lacus ut metus dignissim ullamcorper. Nullam rutrum
+				nisi at magna commodo posuere. Nunc cursus id lacus non molestie. Nulla
+				gravida arcu tortor, elementum pellentesque urna blandit et. Nullam non
+				lacus ac ex aliquet lacinia. Ut et nibh in nisl malesuada gravida. Morbi
+				vel malesuada justo.
+			</p>
+			<p>
+				Etiam vitae metus sed metus scelerisque lobortis. Duis tristique diam at
+				ultricies commodo. Praesent at ex consectetur, malesuada purus quis,
+				hendrerit massa. Suspendisse vehicula rutrum pellentesque. Nam eleifend
+				libero ut dolor molestie fringilla. Fusce efficitur accumsan feugiat.
+				Nulla eget leo sit amet diam elementum semper. Suspendisse varius
+				efficitur pulvinar. In hac habitasse platea dictumst.
+			</p>
+			<p>
+				Quisque pretium fringilla neque, vitae luctus libero tempus sit amet. In
+				consectetur eget augue in scelerisque. Sed ut rhoncus ex. Curabitur
+				condimentum magna in justo sagittis efficitur. Praesent quis augue
+				neque. Quisque vel vulputate purus, ac rhoncus massa. Duis at libero ac
+				arcu rutrum mollis at quis metus.
+			</p>
+		</>
+	);
 };
 
 export default HomePage;
